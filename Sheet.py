15f8a0675e402816e05eb6ca8b48f0f399bc2bbc
me9ded <aminejmal29@gmail.com>
2024-02-01 11:26:15 -0500

    second commit

--- Sheet.py
+++ Sheet.py
@@ -1,53 +1,37 @@
-import argparse
+import sys
 def double_check(val):
-    ivalue=float(val)
-    if 0 <= ivalue <= 9:
-        return ivalue
+    if 0 <= val <= 9:
+        return val
     else:
-        raise argparse.ArgumentTypeError(f'{val} is not in range [0,9]')
-parser=argparse.ArgumentParser(description="calculator")
-parser.add_argument('--number1',type=double_check,required=True,help='enter the first number in [0,9]')
-parser.add_argument('--number2',type=double_check,required=True,help='enter the second numbe in [0,9]')
-parser.add_argument('--operatorselected',type=str,help='Enter the desired operator you want to use')
-args=parser.parse_args()
-number1=args.number1
-number2=args.number2
-op_selected=args.operatorselected
-print(f'Entered numbers:{number1},{number2}')
-def add(N1,N2):
-    a=N1+N2
-    print(f"result: {a}")
-def sub(N1,N2):
-    if N1>=N2:
-        print(f"result: {N1-N2}")
+        raise ValueError(f"{val} not in range[0,9]")
+Number1=float(sys.argv[1])
+Number2=float(sys.argv[2])
+op_selected=str(sys.argv[3])
+def add(n1,n2):
+    print(f" result: {n1+n2}")
+def sub(n1,n2):
+    if n1>n2:
+        print(f"result: {n1-n2}")
     else:
-        print(N2-N1)
-def multiply(N1,N2):
-    if N1==0 or N2==0:
-        print(f"result: {0}")
-    else:
-        print(f"result: {N1*N2}")
-def divide(N1,N2):
-    if N2!=0:
-        print(f"result: {N1/N2}")
-    else:
-        print("Error You cannot divide by 0")
+        print(f"result: {n2-n1}")
+def divide(n1,n2):
+    if n2==0:
+        print("Error cannot divide by 0")
+    return(f"result= {n1/n2}")
+def multiply(n1,n2):
+    print(f"result:{n1*n2}")
 def main():
-    print(f"N1: {args.number1}")
-    print(f"N2: {args.number2}")
-    double_check(args.number1)
-    double_check(args.number2)
+    double_check(Number1)
+    double_check(Number2)
     if op_selected=="add":
-        add(args.number1,args.number2)
+        add(Number1,Number2)
     if op_selected=="sub":
-        sub(args.number1,args.number2)
+        sub(Number1,Number2)
     if op_selected=="multiply":
-        multiply(args.number1,args.number2)
+        multiply(Number1,Number2)
     if op_selected=="divide":
-        divide(args.number1,args.number2)
+        divide(Number1,Number2)
 if __name__=="__main__":
     main()
     exit()
 
-
-    
